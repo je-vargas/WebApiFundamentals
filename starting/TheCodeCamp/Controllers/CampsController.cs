42579df806825d4e857591325dc964fd80c9af79
juan-vargas-salamanca <66321039+juan-vargas-salamanca@users.noreply.github.com>
2020-08-23 15:13:08 +0100

    replaced values controller for campController which can be queried via postman

---
+++ starting/TheCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace TheCodeCamp.Controllers
+{
+    public class CampsController : ApiController
+    {
+        //Handleing Errors this way does not work! as well !! 
+        public object Get()
+        {
+            return new {Name = "Shawn", Occupation = "Teacher "};
+        }
+    }
+}
