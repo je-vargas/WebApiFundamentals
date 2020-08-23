2452658ee3b185d64b733ce5913a1a8a5b422eb4
juan-vargas-salamanca <66321039+juan-vargas-salamanca@users.noreply.github.com>
2020-08-23 15:52:33 +0100

    made get method asynchornous as we are returning data from dataBase within a try and catch

--- starting/TheCodeCamp/Controllers/CampsController.cs
+++ starting/TheCodeCamp/Controllers/CampsController.cs
@@ -1,13 +1,33 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using TheCodeCamp.Data;
 
 namespace TheCodeCamp.Controllers
 {
     public class CampsController : ApiController
     {
-        //Handleing Errors this way does not work! as well !! 
-        public object Get()
+        private readonly ICampRepository _repository;
+        public CampsController(ICampRepository repository)
         {
-            return new {Name = "Shawn", Occupation = "Teacher "};
+            _repository = repository;
         }
+        public async Task<IHttpActionResult> Get()
+        {
+            try
+            {
+                var result = await _repository.GetAllCampsAsync();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                //TODO Add logging 
+                return InternalServerError(ex);
+            }
+       
+        }
+
     }
 }
+
+//IHttpActionResult -> used instead as it returns status codes
